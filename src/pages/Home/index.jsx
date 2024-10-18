e7f0176288fbfe4fc0de21fdbff54906e7ef1bbf
yanca <yancafernandes2@gmail.com>
2024-10-17 21:37:28 -0500

    Estilizando página Home com modelo proposto no figma

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,48 @@
 import React from "react";
 import "./style.css";
+import Logo from "../../components/Logo/Logo";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const goToLogin = () => {
+    navigate("/login");
+  };
+
+  const goToSignUp = () => {
+    navigate("/signup");
+  };
+
   return (
-    <ul className="container">
-      <li>
-        <a className="button" href="/login">
-          Login
-        </a>
-      </li>
-      <li>
-        <a className="button-space" href="/signup">
-          Sign Up
-        </a>
-      </li>
-    </ul>
+    <div className="container">
+      <div className="container-box">
+        <div className="logo-image">
+          <Logo alt="Logo Ifac" />
+        </div>
+
+        <div className="div-home">
+          <div className="titles-home">
+            <h1 className="title">Bem-vindo ao Ifatech</h1>
+            <h2>
+              O sistema que facilita o controle de fichas de lanches,
+              <br />
+              garantindo praticidade e eficiência para alunos e administradores
+            </h2>
+          </div>
+
+          <div>
+            <button className="button-home" onClick={goToLogin}>
+              Entrar
+            </button>
+
+            <button className="button-home" onClick={goToSignUp}>
+              Cadastrar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
