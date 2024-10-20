291e01b22aa27b199fdc4ae8af0467a39647b009
Yanca <yancafernandes2@gmail.com>
2024-10-20 10:33:36 -0500

    Removendo logs de erros

--- src/connections/supabaseClient.js
+++ src/connections/supabaseClient.js
@@ -3,8 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 // Inicialização do cliente Supabase
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Anon Key:", supabaseAnonKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
