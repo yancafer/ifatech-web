ed8d01536e017c26fab146a403352c858817abfc
Yanca <yancafernandes2@gmail.com>
2024-10-19 19:09:11 -0500

    Removendo páginas desnecessárias: admindashboard

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,6 @@ import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 
 import Private from "./Private";
-import AdminDashboard from "../pages/Dashboards/AdminDashboard";
 import CheckQRCode from "../pages/Dashboards/checkQRCode";
 import FilesStudents from "../pages/Dashboards/filesStudents";
 import RegisterStudents from "../pages/Dashboards/registerStudents";
@@ -19,13 +18,10 @@ function RoutesApp() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
 
-
-        <Route path="/admindashboard" element={<Private> <AdminDashboard/> </Private>} />
         <Route path="/checkqrcode" element={<Private> <CheckQRCode/> </Private>} />
         <Route path="/filesstudents" element={<Private> <FilesStudents/> </Private>} />
         <Route path="/registerstudets" element={<Private> <RegisterStudents/> </Private>} />
         <Route path="/reports" element={<Private> <Report/> </Private>} />
-
 
       </Routes>
     </Router>
